667a771c6324aa9c2d0d99284823e73f52496e89
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-09-13 00:11:16 +0530

    updated to Jacob's Name

--- app/(root)/projects/[projectId]/page.tsx
+++ app/(root)/projects/[projectId]/page.tsx
@@ -18,7 +18,7 @@ interface ProjectPageProps {
   };
 }
 
-const githubUsername = "namanbarkiya";
+const githubUsername = "jacobjerryarackal";
 
 export default function Project({ params }: ProjectPageProps) {
   let project = Projects.find((val) => val.id === params.projectId);
@@ -79,7 +79,7 @@ export default function Project({ params }: ProjectPageProps) {
             />
 
             <div className="flex-1 text-left leading-tight">
-              <p className="font-medium">{"Naman Barkiya"}</p>
+              <p className="font-medium">{"Jacob Jerry Arackal"}</p>
               <p className="text-[12px] text-muted-foreground">
                 @{siteConfig.username}
               </p>
