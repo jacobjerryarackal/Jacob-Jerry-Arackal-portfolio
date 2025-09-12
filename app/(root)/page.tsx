9f8bd722d85fc0b850ed86bb71228dca2726068a
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-09-12 23:36:11 +0530

    Added name and description of Jacob

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -81,7 +81,7 @@ export default function IndexPage() {
             width={100}
             sizes="100vw"
             className="bg-primary rounded-full mb-0 h-auto md:mb-2 w-[60%] max-w-[16rem] border-8 border-primary"
-            alt="Naman Barkiya - Full Stack Developer Portfolio"
+            alt="Jacob Jerry Arackal - Full Stack Developer Portfolio"
             priority
           />
           <AnimatedText
@@ -89,7 +89,7 @@ export default function IndexPage() {
             delay={0.2}
             className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
           >
-            Naman Barkiya
+            Jacob Jerry Arackal
           </AnimatedText>
           <AnimatedText
             as="h3"
@@ -100,18 +100,18 @@ export default function IndexPage() {
           </AnimatedText>
           <div className="mt-4 max-w-[42rem] text-center">
             <p className="leading-normal text-muted-foreground text-sm sm:text-base">
-              Open-source Next.js portfolio template. Fork this on GitHub to
-              create your own developer portfolio.
+              A highly motivated Full Stack Engineer with a strong passion for developing scalable web and AI-driven applications. 
+              I specialize in Next.js, Python, and the MERN stack, leveraging modern technologies to solve complex challenges and drive user engagement.
             </p>
           </div>
 
           <div className="flex flex-col mt-10 items-center justify-center sm:flex-row sm:space-x-4 gap-3">
             <AnimatedText delay={0.6}>
               <Link
-                href={"https://github.com/namanbarkiya"}
+                href={"https://github.com/jacobjerryarackal"}
                 target="_blank"
                 className={cn(buttonVariants({ size: "lg" }))}
-                aria-label="View Naman Barkiya's GitHub profile"
+                aria-label="View Jacob Jerry Arackal's GitHub profile"
               >
                 <Icons.gitHub className="w-4 h-4 mr-2" /> GitHub
               </Link>
