01b84c35037a3d51bdd9077ccf417c8804c6677d
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-09-13 18:42:18 +0530

    updated to jacob's links

--- config/site.ts
+++ config/site.ts
@@ -4,16 +4,16 @@ export const siteConfig = {
   username: "jacobjerryarackal",
   description:
     "I'm a Full-Stack Developer (1+ year) building production web apps with Next.js, Node.js and MongoDB. I focus on booking systems, authentication (OAuth), payments and clean, maintainable code. Open to freelance & full-time roles.",
-  url: "https://nbarkiya.xyz",
+  url: "http://localhost:3000",// or "https://nbarkiya.xyz"
   links: {
-    twitter: "https://x.com/namanbarkiya",
+    linkedin: "https://www.linkedin.com/in/jacob-jerry-arackal-5a4596190/",
     github: "https://github.com/jacobjerryarackal",
   },
-  ogImage: "https://nbarkiya.xyz/profile-img.jpg",
+  ogImage: "/public/profile-img.jpg",
   iconIco:
     "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357384/assets/naman-favicon.ico",
   logoIcon:
-    "https://res.cloudinary.com/dbfvcn3f6/image/upload/v1692357294/assets/naman-logo.png",
+    "/public/logo.jpg",
   keywords: [
     "Jacob Jerry Arackal",
     "Full Stack Developer",
