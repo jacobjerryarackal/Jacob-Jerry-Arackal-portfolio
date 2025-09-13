4f15d30668944370e31a4434a6a6bb2bb3e0117c
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-09-13 18:45:45 +0530

    Commented Contributions so that I can add them later

--- config/contributions.ts
+++ config/contributions.ts
@@ -5,29 +5,32 @@ export interface contributionsInterface {
   link: string;
 }
 
-export const contributionsUnsorted: contributionsInterface[] = [
-  {
-    repo: "autogen",
-    contibutionDescription:
-      "Improved the gallery component to showcase the community work.",
-    repoOwner: "Microsoft",
-    link: "https://github.com/microsoft/autogen/pull/1445",
-  },
-  {
-    repo: "creativecommons",
-    contibutionDescription:
-      "Closed Issue: Fixed navbar issue on the main website of creative common.",
-    repoOwner: "Creative Commons",
-    link: "https://github.com/creativecommons/creativecommons.github.io-source/pull/738",
-  },
-  {
-    repo: "creativecommons",
-    contibutionDescription:
-      "Added section for 'Other Opportunities' on main page.",
-    repoOwner: "Creative Commons",
-    link: "https://github.com/creativecommons/creativecommons.github.io-source/pull/719",
-  },
-];
+export const contributionsUnsorted: contributionsInterface[] = [];
+
+
+// export const contributionsUnsorted: contributionsInterface[] = [
+//   {
+//     repo: "autogen",
+//     contibutionDescription:
+//       "Improved the gallery component to showcase the community work.",
+//     repoOwner: "Microsoft",
+//     link: "https://github.com/microsoft/autogen/pull/1445",
+//   },
+//   {
+//     repo: "creativecommons",
+//     contibutionDescription:
+//       "Closed Issue: Fixed navbar issue on the main website of creative common.",
+//     repoOwner: "Creative Commons",
+//     link: "https://github.com/creativecommons/creativecommons.github.io-source/pull/738",
+//   },
+//   {
+//     repo: "creativecommons",
+//     contibutionDescription:
+//       "Added section for 'Other Opportunities' on main page.",
+//     repoOwner: "Creative Commons",
+//     link: "https://github.com/creativecommons/creativecommons.github.io-source/pull/719",
+//   },
+// ];
 
 export const featuredContributions: contributionsInterface[] =
   contributionsUnsorted.slice(0, 3);
